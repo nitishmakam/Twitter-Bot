d1901d27be6bdc6020c0e83f391cf7e17dffaf6f
Nitish J Makam <nitish.j@Admins-MacBook-Pro.local>
2019-11-29 11:34:06 +0530

    Adding missing import for sys. Adding logging

--- unlike_tweets.py
+++ unlike_tweets.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import time
 import configparser
 import os
+import sys
+import logging
 
 driver = webdriver.Chrome()
 
@@ -31,57 +33,70 @@ class SeleniumActions:
 sel = SeleniumActions()
 
 class Twitter:
-	domain_name = "https://twitter.com"
+	domain_name = "https://www.twitter.com"
 
 	config = None
 	cfg_fname = 'config.ini'
 
 	username = ''
 	password = ''
+	logger = None
 	
 	def config_init(self, cfg_fname):
+		self.logger.debug("Fetching config details")
 		curr_dir = os.getcwd()
-		cfg_fpath = os.path.join(curr_dir,cfg_fname)
+		cfg_fpath = os.path.join(curr_dir, cfg_fname)
 	
 		if not os.path.exists(cfg_fpath):
-			print("Config path doesn't exist")
+			self.logger.error("Config path doesn't exist")
 			sys.exit(1)
 
 		self.config = configparser.ConfigParser()
 		self.config.read(cfg_fpath)
+
+
+	def logger_init(self):
+		self.logger = logging.getLogger()
+		self.logger.setLevel(logging.DEBUG)
 
 	def logout(self):
 		time.sleep(10)
 		driver.close()
 	
 	def unlike_tweets(self):
-		driver.get("{}/{}/likes".format(self.domain_name,self.username))
-			
+		xpath_for_like_button = "/html/body/div/div/div/div/main/div/div/div/div/div/div[2]/div/div/div[2]/section/div/div/div/div[{}]/div/article/div/div[2]/div[2]/div[{}]/div[3]/div/div/div[1]"
+		
 		for j in range(1,10):
-			driver.get("{}/{}/likes".format(self.domain_name,self.username))
+			# Fetch likes page periodically because Twitter doesn't autoload and to avoid all the extra processing for scrolling down
+			self.logger.debug("Fetching likes page")
+			driver.get("{}/{}/likes".format(self.domain_name, self.username))
+			#Wait for page to load
 			time.sleep(5)
+
 			for i in range(1,10):
+				
 				try:
-					k=3
-					path = "/html/body/div/div/div/div/main/div/div/div/div/div/div[2]/div/div/div[2]/section/div/div/div/div[{pos}]/div/article/div/div[2]/div[2]/div[{sec}]/div[3]/div/div/div[1]".format(pos=i,sec=k)
-					print("1-"+path)
+					
+					k = 3
+					path = xpath_for_like_button.format(i, k)
 					sel.act_btn(xpath = path, action='click')
-					time.sleep(2)
-					print("Trying first - "+(j*10+i))
-				except Exception:
+				
+				except Exception as e:
 					print("Caught exception")
+					self.logger.error("No such element found at path - %s", path)
 					time.sleep(1)
         
 					try:    
-						k=4
-						path="/html/body/div/div/div/div/main/div/div/div/div/div/div[2]/div/div/div[2]/section/div/div/div/div[{pos}]/div/article/div/div[2]/div[2]/div[{sec}]/div[3]/div/div/div[1]".format(pos=i,sec=k)
-						print("2-"+path)
+						
+						k = 4
+						path = xpath_for_like_button.format(i, k)
 						sel.act_btn(xpath = path, action='click')
-						print("Trying second - " +(j*10+i))
-						time.sleep(2)
-					except Exception:
+					
+					except Exception as e:
 						print("Caught exception 2")
 						time.sleep(1)
+				
+				time.sleep(2)
 
 	def login_app(self,username,password):				
 		driver.get(self.domain_name)
@@ -96,18 +111,23 @@ class Twitter:
 		#passwordField=driver.find_element_by_css_selector("div#page-container > div > div > form > fieldset > div:nth-of-type(2) > input")
 		#passwordField.send_keys(password)
 
+		self.logger.debug("Performing login for user - %s",self.username)
+
 		sel.act_btn(tag_name = "button", action='click')
 
 	def execute(self):
 		
+		#Logging init
+		self.logger_init()
+
 		#Config init
 		self.config_init(self.cfg_fname)
 
 		#Load credentials
-		self.username = self.config.get("twitter","username")
-		self.password = self.config.get("twitter","password")
+		self.username = self.config.get("twitter", "username")
+		self.password = self.config.get("twitter", "password")
 
-		self.login_app(self.username,self.password)
+		self.login_app(self.username, self.password)
 		self.unlike_tweets()
 		self.logout()	
 
